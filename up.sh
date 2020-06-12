ffff6a29f532623a3511856be00e01a695f69774
Joshua Gilman <joshuagilman@gmail.com>
2020-06-12 10:38:20 -0700

    Adds custom inventory configuration to allow more customization when configuring via kubespray

--- up.sh
+++ up.sh
@@ -27,34 +27,31 @@ fi
 
 echo "$2"
 if [[ ! "$2" == "--only-provision" ]]; then
-    echo "Bringing up cluster..."
-    pulumi up -y
+#    echo "Bringing up cluster..."
+#    pulumi up -y
     checkForError $? "failed bringing up cluster infrastrucure, cluster may be in an incomplete state"
 fi
+
+name=$(pulumi stack output cluster | jq -r .name)
+env_name=$(pulumi stack output environment | jq -r .name)
+master=$(pulumi stack output cluster | jq -r .masters[0])
 
 echo "Pulling down kubespray..."
 rm -rf /tmp/kubespray &> /dev/null
 git clone https://github.com/kubernetes-sigs/kubespray /tmp/kubespray
 
-echo "Cloning inventory..."
-cp -r /tmp/kubespray/inventory/sample /tmp/kubespray/inventory/glab
+echo "Copying inventory over..."
+cp -r inv/${env_name} /tmp/kubespray/inventory
 
 echo "Creating inventory file..."
-pulumi stack output cluster | python inv.py > /tmp/kubespray/inventory/glab/inventory.ini
+pulumi stack output cluster | python inv.py > /tmp/kubespray/inventory/${env_name}/inventory.ini
 checkForError $? "failed generating Ansible inventory"
 
-echo "Setting cluster name..."
-name=$(pulumi stack output cluster | jq -r .name)
-sed -i "" -e "s/cluster.local/$name/" /tmp/kubespray/inventory/glab/group_vars/k8s-cluster/k8s-cluster.yml
-checkForError $? "failed setting the cluster name"
-
 echo "Configuring cluster..."
-ansible-playbook -i /tmp/kubespray/inventory/glab/inventory.ini --become --become-user=root /tmp/kubespray/cluster.yml
+ansible-playbook -i /tmp/kubespray/inventory/${env_name}/inventory.ini --become --become-user=root /tmp/kubespray/cluster.yml
 checkForError "failed provisioning the cluster, it may be in an incomplete state"
 
 echo "Pulling down kube config..."
-env_name=$(pulumi stack output environment | jq -r .name)
-master=$(pulumi stack output cluster | jq -r .masters[0])
 mkdir -p ~/.kube/custom-contexts/${name}
 ssh -t josh@${master} "sudo cat /etc/kubernetes/admin.conf" > ~/.kube/custom-contexts/${name}/config.yml
 
